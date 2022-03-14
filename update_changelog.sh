a90ca7e23c85c026bdfa41b733c9698f487fe43d
cindy zhao <xinyuezhao96@gmail.com>
2022-03-14 14:13:54 -0700

    config user email&name

--- update_changelog.sh
+++ update_changelog.sh
@@ -8,11 +8,14 @@ cd $1
 antsibull-changelog release -v
 
 # 2. Push updated files to repo
+git config user.email "dcn-ecosystem@cisco.com"
+git config user.name "dcn-ecosystem"
 cat ./changelogs/changelog.yaml
 git add -u
 git status
 git commit -m 'Update change log & galaxy.yml'
 # push branch to the remote repo where you want to create the PR
+# git push --set-upstream origin changelog
 git push -f $3
 git clean -f -d
 
